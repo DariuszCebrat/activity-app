980ed26e7d9a737224fbb71fd6717561ad4fe94a
Darius Cebrat <dcmieczyslaw@gmail.com>
2024-08-27 17:06:10 +0200

    adding repository

---
+++ activity.domain/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using activity.domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity.domain.Interfaces.Repository
+{
+    public interface IRepositoryBase<Entitie> where Entitie : RepositoryEntitie
+    {
+    }
+}
---
+++ activity.infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using activity.domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity.infrastructure.Repositories
+{
+    public class RepositoryBase<Entitie>where Entitie : RepositoryEntitie
+    {
+        private DataContext _context;
+        private DbSet<Entitie> _entitie;
+        public RepositoryBase(DataContext context)
+        {
+            _context = context; 
+            _entitie = _context.Set<Entitie>();
+        }
+        public IQueryable<Entitie> GetAll()
+        {
+            return _entitie.AsQueryable();
+        }
+        public  async Task<Entitie> GetAsync(Guid id)
+        {
+           var entitie =await  _entitie.FirstOrDefaultAsync(x => x.Id == id);
+           if (entitie is null)
+                throw new NotFoundException();
+
+            return entitie;
+        }
+
+
+    }
+}

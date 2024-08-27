dedeec2378e03fd87640fea9c195904f3ae903e1
Darius Cebrat <dcmieczyslaw@gmail.com>
2024-08-27 10:15:08 +0200

    adding entitie activity,dbcontext , migration

---
+++ shop.infrastructure/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shop.domain.Entities;
+
+namespace shop.infrastructure
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}

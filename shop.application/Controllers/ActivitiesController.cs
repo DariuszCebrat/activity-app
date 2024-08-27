d97fa031e1ac6afddab0ad73f4c84274cea03ad5
Darius Cebrat <dcmieczyslaw@gmail.com>
2024-08-27 10:47:13 +0200

    adding activities controller with get functions

---
+++ shop.application/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using activity.domain.Entities;
+using activity.infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace activity.application.Controllers
+{
+
+    public class ActivitiesController:BaseApiController
+    {
+        private DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return Ok(await _context.Activities.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity([FromRoute] Guid id)
+        {
+            var activity = await _context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+            if (activity == null)
+                return NotFound();
+            return Ok(activity);
+        }
+
+    }
+}
---
+++ shop.application/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace activity.application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController:ControllerBase
+    {
+    }
+}
